c1dec4b11845cec37e76c5fe9f3b0c767889e891
ridcully <ridcully>
2004-02-27 17:06:25 +0000

    - inital add for the unit tests

---
+++ tests.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import unittest
+import nidhoeggr
+import config
+from socket import socket, AF_INET, SOCK_STREAM
+
+class RacelistServerNetworkTest(unittest.TestCase):
+
+	def test(self):
+		self.assertEqual(1,1)
+
+class BroadcastNetworkServerTest(unittest.TestCase):
+
+	def test(self):
+		self.assertEqual(1,1)
+
+class ScaryProtocolTest(unittest.TestCase):
+
+	def test(self):
+		self.assertEqual(1,1)
+
+if __name__=='__main__':
+	unittest.main()
